206298c0e5fac4c12159e6a2278cc2f1d2ec92f5
yatharthvishwa <yatharth4665@gmail.com>
2025-11-07 17:05:25 +0530

    refactor

--- ASCII_SHADER/ASCII_SHADER.py
+++ ASCII_SHADER/ASCII_SHADER.py
@@ -12,17 +12,16 @@ asciilen = len(asciichars)
 print(imageheight)
 print(imagewidth)
 
-
+def get_asciichar(brightness):
+    #I need to map 0-255 brightness to 0-asciilen
+    unit = 256 / asciilen
+    ascii_index = int(brightness / unit)
+    return asciichars[ascii_index]
 
 for i in range(imageheight):
     for j in range(imagewidth):
         r, g, b = imageload[j,i] #imageload[j, i] accesses the pixel at column j, row i (note: it's [x, y] format), This returns a tuple like (255, 128, 0) containing three values
         brightness = int(r/3 + g/3 + b/3) #take rgb average for brigtness value
         imageload[j,i] = (brightness, brightness, brightness)
-
-        #I need to map 0-255 brightness to 0-asciilen
-        unit = 256 / asciilen
-        ascii_index = int(brightness / unit)
-        imageload[j,i] = (ascii_index, ascii_index, ascii_index)
 
 imagevar.save("output.jpeg")
