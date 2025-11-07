be51b6fd46ef773e7cb17273c6b9bf308634ce43
yatharthvishwa <yatharth4665@gmail.com>
2025-11-07 17:02:37 +0530

    implemented mapping logic

--- ASCII_SHADER/ASCII_SHADER.py
+++ ASCII_SHADER/ASCII_SHADER.py
@@ -5,13 +5,24 @@ imageheight = imagevar.height
 
 imageload = imagevar.load() #load() method is used to explicitly load the image data into memory when you call image.open it just reads the file header and necessary metadata not the entire image data
 
+asciichars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,. "
+asciilen = len(asciichars)
+
+
 print(imageheight)
 print(imagewidth)
+
+
 
 for i in range(imageheight):
     for j in range(imagewidth):
         r, g, b = imageload[j,i] #imageload[j, i] accesses the pixel at column j, row i (note: it's [x, y] format), This returns a tuple like (255, 128, 0) containing three values
-        h = int(r/3 + g/3 + b/3) #take rgb average
-        imageload[j,i] = (h, h, h) 
+        brightness = int(r/3 + g/3 + b/3) #take rgb average for brigtness value
+        imageload[j,i] = (brightness, brightness, brightness)
+
+        #I need to map 0-255 brightness to 0-asciilen
+        unit = 256 / asciilen
+        ascii_index = int(brightness / unit)
+        imageload[j,i] = (ascii_index, ascii_index, ascii_index)
 
 imagevar.save("output.jpeg")
