1c503606ac53b14bd254d1a8788407db0cbb29da
yatharthvishwa <yatharth4665@gmail.com>
2025-11-07 17:16:43 +0530

    completed ascii algorithm without scaling

--- ASCII_SHADER/ASCII_SHADER.py
+++ ASCII_SHADER/ASCII_SHADER.py
@@ -5,9 +5,10 @@ imageheight = imagevar.height
 
 imageload = imagevar.load() #load() method is used to explicitly load the image data into memory when you call image.open it just reads the file header and necessary metadata not the entire image data
 
-asciichars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,. "
+asciichars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,. " [::-1]
 asciilen = len(asciichars)
 
+textfile = open("output.txt", "w") 
 
 print(imageheight)
 print(imagewidth)
@@ -23,5 +24,7 @@ for i in range(imageheight):
         r, g, b = imageload[j,i] #imageload[j, i] accesses the pixel at column j, row i (note: it's [x, y] format), This returns a tuple like (255, 128, 0) containing three values
         brightness = int(r/3 + g/3 + b/3) #take rgb average for brigtness value
         imageload[j,i] = (brightness, brightness, brightness)
+        textfile.write(get_asciichar(brightness))
+    textfile.write("\n")
 
 imagevar.save("output.jpeg")
